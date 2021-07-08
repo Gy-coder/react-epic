ab3b44837a5a367d297adabd8a55835127d4edc9
Gy <406631892@qq.com>
2021-07-08 19:57:28 +0800

    login完成

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,20 +1,77 @@
-import { observer } from "mobx-react";
-import React, { useState } from "react";
-import { useStores } from "../stores";
+import React from "react";
+import styled from "styled-components";
+import { Form, Input, Button } from "antd";
 
-const Login = observer(() => {
-  const { AuthStore } = useStores();
-  const [x, setX] = useState("");
-  const handleChage = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    await setX((prev) => (prev = e.target.value));
-    AuthStore.setUsername(e.target.value);
+const Wrapper = styled.div`
+  max-width: 600px;
+  margin: 30px auto;
+  box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 30px;
+`;
+
+const Register: React.FC = () => {
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  const validateUsername = (rule: any, value: string) => {
+    if (/\W/.test(value)) return Promise.reject("只能是数字、字母或下划线");
+    if (value.length < 4 || value.length > 10)
+      return Promise.reject("长度为4～10个字符");
+    return Promise.resolve();
   };
   return (
-    <>
-      <h1>AuthStore: {AuthStore.values.username}</h1>
-      <input type="text" value={x} onChange={handleChage} />
-    </>
-  );
-});
+    <Wrapper>
+      <Title>登陆</Title>
+      <Form
+        name="basic"
+        labelCol={{ span: 6 }}
+        wrapperCol={{ span: 18 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="用户名"
+          name="username"
+          rules={[
+            { required: true, message: "输入用户名" },
+            { validator: validateUsername },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-export default Login;
+        <Form.Item
+          label="密码"
+          name="password"
+          rules={[
+            { required: true, message: "输入密码" },
+            { min: 4, message: "最少4个字符" },
+            { max: 16, message: "最多不超过16个字符" },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 12, span: 12 }}>
+          <Button type="primary" htmlType="submit">
+            提交
+          </Button>
+        </Form.Item>
+      </Form>
+    </Wrapper>
+  );
+};
+
+export default Register;
